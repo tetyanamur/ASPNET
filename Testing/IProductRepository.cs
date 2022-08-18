21e0427964c49d11673d7d772a4119ce01b0032f
Tetyana Murzina <tetyanamur@gmail.com>
2022-08-17 20:03:21 -0500

    initial commit

---
+++ Testing/IProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Testing.Models;
+using Dapper;
+
+namespace Testing
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+        public Product GetProduct(int id);
+        public void UpdateProduct(Product product);
+        public void InsertProduct(Product productToInsert);
+        public IEnumerable<Category> GetCategories();
+        public Product AssignCategory();
+        public void DeleteProduct (Product product);
+
+
+    }
+}
---
+++ Testing/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Testing.Models;
+
+namespace Testing
+{
+    public class Product
+    {
+        public Product()
+        {
+
+        }
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int CategoryID { get; set; }
+        public int OnSale { get; set; }
+        public string StockLevel { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+    }
+}
